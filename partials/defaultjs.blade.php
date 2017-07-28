f34f6e38a3dc389ba07c7083c3898e714270eba9
GYO\Jarvis <gistha.yogaswara>
2017-07-28 16:32:34 +0700

    update validation input qty

--- partials/defaultjs.blade.php
+++ partials/defaultjs.blade.php
@@ -1,2 +1,3 @@
 <!-- Default js -->
-<script data-main="//s3-ap-southeast-1.amazonaws.com/cdn2.jarvis-store.com/assets/tender/assets/js/app-main" src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.22/require.min.js"></script>
+<script data-main="{{url(dirTemaToko().'tender/assets/js/app-main')}}" src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.22/require.min.js"></script>
+<!-- <script data-main="//s3-ap-southeast-1.amazonaws.com/cdn2.jarvis-store.com/assets/tender/assets/js/app-main" src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.22/require.min.js"></script> -->
